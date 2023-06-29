1337aa164928467e599348d16eb23963b2ab9823
1F10220059 <s1f102200592@iniad.org>
2023-06-29 12:08:35 +0900

    トップページを追加

--- todo/tests.py
+++ todo/tests.py
@@ -50,4 +50,4 @@ class TaskModelTestCase(TestCase):
         task = Task(title='task3', due_at=due)
         task.save()
 
-        self.assertFalse(task.is_overdue(current))
+        self.assertFalse(task.is_overdue(current))      
