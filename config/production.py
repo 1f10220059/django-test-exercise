fa275effe46b352d3481d02fded82f3544304e44
1F10220059 <s1f102200592@iniad.org>
2023-07-13 11:54:46 +0900

    renderを更新

--- config/production.py
+++ config/production.py
@@ -9,7 +9,7 @@ ALLOWED_HOSTS.append(os.environ['RENDER_EXTERNAL_HOSTNAME'])
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 DATABASES = {
-    'default':dj_database_url.config()
+    'default': dj_database_url.config()
 }
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'Whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
